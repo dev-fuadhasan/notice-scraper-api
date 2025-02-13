36c681bcd53d1f838ac96471588e50ec57f23c5b
dev-fuadhasan <137803065+dev-fuadhasan@users.noreply.github.com>
2025-02-13 17:49:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from scrapper import NoticeScraper  # Import the scraper class from scrapper.py
 
@@ -15,4 +16,6 @@ def get_notices():
     return jsonify(notices), 200  # Return notices as JSON response
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # Use the port from the environment variable, or default to 5000 for local development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)  # Use dynamic port binding
